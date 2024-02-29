fe8018b094eed0acc600d554b1b5972edc3e4105
Shibo Gao <happygaoshibo@outlook.com>
2024-02-29 12:54:34 +0800

    Create 1-2.sh

---
+++ Scripts/1-2/1-2.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+#     Operation System Course's Educoder excrises shell script. / 操作系统课程的头歌过关脚本。
+#     Copyright (C) 2024  gaobobo
+
+readonly DownloadSite=${1:-Gitee} 
+
+echo "Hello,Linux!" > /home/headless
+
+exit 0
