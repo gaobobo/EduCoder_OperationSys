0f84d445d53dddfddcf676625a8313f0c4f4d7b5
Shibo Gao <happygaoshibo@outlook.com>
2024-02-29 13:09:01 +0800

    Add 1-4

---
+++ Scripts/1-4/1-4.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+#     Operation System Course's Educoder excrises shell script. / 操作系统课程的头歌过关脚本。
+#     Copyright (C) 2024  gaobobo
+
+readonly DownloadSite=${1:-Gitee}
+
+case $DownloadSite in
+    Gitee)
+        wget -O /tmp/1-4/hd.c https://gitee.com/coconut_floss/EduCoder_OperationSys/raw/main/Source/1-4/hd.c
+        ;;
+    Github)
+        wget -O /tmp/1-4/hd.c https://raw.githubusercontent.com/gaobobo/EduCoder_OperationSys/main/Source/1-4/hd.c
+        ;;
+    *)
+        echo "Unknown download site: $DownloadSite"
+        echo "未知的下载站点：$DownloadSite"
+        exit 1
+        ;;
+esac
+
+mv -f /tmp/1-4/hd.c /home/headless/os/linux-0.11-lab/0/linux/kernel/blk_drv/hd.c
+
+cd /home/headless/os/linux-0.11-lab/0/linux/
+make
+
+exit 0
