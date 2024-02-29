a44612a966c9c1154b00a77fbc6d2a850c7c71b6
Shibo Gao <happygaoshibo@outlook.com>
2024-02-29 14:21:39 +0800

    Update 1-4.sh

--- Scripts/1-4/1-4.sh
+++ Scripts/1-4/1-4.sh
@@ -19,6 +19,12 @@ case $DownloadSite in
         ;;
 esac
 
+if [ $? -ne 0 ]; then
+    echo "Download ${LevelCode}.sh failed. Check netork."
+    echo "下载 ${LevelCode}.sh 失败。请检查网络。"
+    exit 1
+fi
+
 mv -f /tmp/1-4/hd.c /home/headless/os/linux-0.11-lab/0/linux/kernel/blk_drv/hd.c
 
 cd /home/headless/os/linux-0.11-lab/0/linux/
