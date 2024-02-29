792c41b16f2c65d9a7826d4fa3025a00e5fc23f5
Shibo Gao <happygaoshibo@outlook.com>
2024-02-29 16:10:45 +0800

    update 2.1.kehou-2

---
+++ Scripts/2.1.kehou-2/2.1.kehou-2.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+#     Operation System Course's Educoder excrises shell script. / 操作系统课程的头歌过关脚本。
+#     Copyright (C) 2024  gaobobo
+
+DownloadSite=${1:-Gitee}
+readonly DownloadSite
+
+mkdir -p /tmp/2.1.kehou-2
+
+case $DownloadSite in
+    Gitee)
+        wget -O /tmp/2.1.kehou-2/sched.c https://gitee.com/coconut_floss/EduCoder_OperationSys/raw/main/Source/2.1.kehou-2/sched.c
+        ;;
+    Github)
+        wget -O /tmp/2.1.kehou-2/sched.c https://raw.githubusercontent.com/gaobobo/EduCoder_OperationSys/main/Source/2.1.kehou-2/sched.c
+        ;;
+    *)
+        echo "Unknown download site: $DownloadSite"
+        echo "未知的下载站点：$DownloadSite"
+        return 1
+        ;;
+esac
+
+rm -f /tmp/2.1.kehou-2/sched.c /root/os/linux-0.11-lab/0/linux/kernel/sched.c
+
+make ~/os/linux-0.11-lab/0/linux/
+
+return 0
