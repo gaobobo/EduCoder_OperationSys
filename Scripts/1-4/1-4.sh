23712903d124767bbc28ad76d56c49f30c90da1f
Shibo Gao <happygaoshibo@outlook.com>
2024-02-29 14:26:21 +0800

    Update 1-4.sh

--- Scripts/1-4/1-4.sh
+++ Scripts/1-4/1-4.sh
@@ -22,7 +22,7 @@ esac
 if [ $? -ne 0 ]; then
     echo "Download hd.c failed. Check netork."
     echo "下载 hd.c 失败。请检查网络。"
-    exit 1
+    return 1
 fi
 
 mv -f /tmp/1-4/hd.c /home/headless/os/linux-0.11-lab/0/linux/kernel/blk_drv/hd.c
