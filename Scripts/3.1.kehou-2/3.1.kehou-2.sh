bee956b56beb6da583db011e948c2dda5860580c
Shibo Gao <happygaoshibo@outlook.com>
2024-03-04 11:00:17 +0800

    update3.1.kehou-2

---
+++ Scripts/3.1.kehou-2/3.1.kehou-2.sh
@@ -0,0 +1,30 @@
+#!/bin/sh第四关
+
+#     Operation System Course's Educoder excrises shell script. / 操作系统课程的头歌过关脚本。
+#     Copyright (C) 2024  gaobobo
+
+DownloadSite=${1:-Gitee}
+readonly DownloadSite
+
+mkdir -p /tmp/3.1.kehou-2
+
+case $DownloadSite in
+    Gitee)
+        wget -O /tmp/3.1.kehou-2/第四关.txt https://gitee.com/coconut_floss/EduCoder_OperationSys/raw/main/Source/3.1.kehou-2/第四关.txt
+        wget -O /tmp/3.1.kehou-2/gdb.log https://gitee.com/coconut_floss/EduCoder_OperationSys/raw/main/Source/3.1.kehou-2/gdb.log
+        ;;
+    Github)
+        wget -O /tmp/3.1.kehou-2/第四关.txt https://raw.githubusercontent.com/gaobobo/EduCoder_OperationSys/main/Source/3.1.kehou-2/第四关.txt
+        wget -O /tmp/3.1.kehou-2/gdb.log https://raw.githubusercontent.com/gaobobo/EduCoder_OperationSys/main/Source/3.1.kehou-2/gdb.log
+        ;;
+    *)
+        echo "Unknown download site: $DownloadSite"
+        echo "未知的下载站点：$DownloadSite"
+        return 1
+        ;;
+esac
+
+mv -f /tmp/3.1.kehou-2/第四关.txt /data/workspace/myshixun/第四关.txt
+mv -f /tmp/3.1.kehou-2/gdb.log /root/gdb.log
+
+return 0
