c3e1c9f79ff5333c2a47ba3a6065bf83201f05e0
Shibo Gao <happygaoshibo@outlook.com>
2024-03-22 21:19:10 +0800

    fix #26

--- Scripts/3.3.kehou-2/3.3.kehou-2.sh
+++ Scripts/3.3.kehou-2/3.3.kehou-2.sh
@@ -10,12 +10,12 @@ mkdir -p /tmp/3.3.kehou-2
 
 case $DownloadSite in
     Gitee)
-        wget -O /tmp/3.3.kehou-2/1.log https://gitee.com/coconut_floss/EduCoder_OperationSys/raw/main/Source/3.3.kehou-1/1.log
-        wget -O /tmp/3.3.kehou-2/sched.c https://gitee.com/coconut_floss/EduCoder_OperationSys/raw/main/Source/3.3.kehou-1/sched.c
+        wget -O /tmp/3.3.kehou-2/1.log https://gitee.com/coconut_floss/EduCoder_OperationSys/raw/main/Source/3.3.kehou-2/1.log
+        wget -O /tmp/3.3.kehou-2/sched.c https://gitee.com/coconut_floss/EduCoder_OperationSys/raw/main/Source/3.3.kehou-2/sched.c
         ;;
     Github)
-        wget -O /tmp/3.3.kehou-2/1.log https://raw.githubusercontent.com/gaobobo/EduCoder_OperationSys/main/Source/3.3.kehou-1/1.log
-        wget -O /tmp/3.3.kehou-2/sched.c https://raw.githubusercontent.com/gaobobo/EduCoder_OperationSys/main/Source/3.3.kehou-1/sched.c
+        wget -O /tmp/3.3.kehou-2/1.log https://raw.githubusercontent.com/gaobobo/EduCoder_OperationSys/main/Source/3.3.kehou-2/1.log
+        wget -O /tmp/3.3.kehou-2/sched.c https://raw.githubusercontent.com/gaobobo/EduCoder_OperationSys/main/Source/3.3.kehou-2/sched.c
         ;;
     *)
         echo "Unknown download site: $DownloadSite"
