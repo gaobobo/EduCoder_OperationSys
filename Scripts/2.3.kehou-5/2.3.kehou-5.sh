1d72ae867c9e536278b2f2086d58b69ec7cd09ea
Shibo Gao <happygaoshibo@outlook.com>
2024-03-28 15:23:58 +0800

    fix bug by 1.log exist

--- Scripts/2.3.kehou-5/2.3.kehou-5.sh
+++ Scripts/2.3.kehou-5/2.3.kehou-5.sh
@@ -10,12 +10,10 @@ mkdir -p /tmp/2.3.kehou-5
 
 case $DownloadSite in
     Gitee)
-        wget -O /tmp/2.3.kehou-5/1.log https://gitee.com/coconut_floss/EduCoder_OperationSys/raw/main/Source/2.3.kehou-5/1.log
         wget -O /tmp/2.3.kehou-5/rootimage-0.11 https://gitee.com/coconut_floss/EduCoder_OperationSys/raw/main/Source/2.3.kehou-5/rootimage-0.11
         wget -O /tmp/2.3.kehou-4.sh https://gitee.com/coconut_floss/EduCoder_OperationSys/raw/main/Scripts/2.3.kehou-4/2.3.kehou-4.sh
         ;;
     Github)
-        wget -O /tmp/2.3.kehou-5/1.log https://raw.githubusercontent.com/gaobobo/EduCoder_OperationSys/main/Source/2.3.kehou-5/1.log
         wget -O /tmp/2.3.kehou-5/rootimage-0.11 https://raw.githubusercontent.com/gaobobo/EduCoder_OperationSys/main/Source/2.3.kehou-5/rootimage-0.11
         wget -O /tmp/2.3.kehou-4.sh https://raw.githubusercontent.com/gaobobo/EduCoder_OperationSys/main/Scripts/2.3.kehou-4/2.3.kehou-4.sh
         ;;
@@ -29,7 +27,8 @@ esac
 chmod +x /tmp/2.3.kehou-4.sh
 /tmp/2.3.kehou-4.sh $DownloadSite
 
+rm -rf /root/1.log
+
 mv -f /tmp/2.3.kehou-5/rootimage-0.11 /home/headless/os/linux-0.11-lab/images/rootimage-0.11
-mv -f /tmp/2.3.kehou-5/1.log /root/1.log
 
 return 0
