40e93ff103b9e2a70b5df41df2aa13030d055468
Shibo Gao <happygaoshibo@outlook.com>
2024-03-28 14:36:22 +0800

    fix wrong file path

--- Scripts/2.3.kehou-3/2.3.kehou-3.sh
+++ Scripts/2.3.kehou-3/2.3.kehou-3.sh
@@ -6,17 +6,17 @@
 DownloadSite=${1:-Gitee}
 readonly DownloadSite
 
-mkdir -p /tmp/2.3.kehou-2
+mkdir -p /tmp/2.3.kehou-3
 
 case $DownloadSite in
     Gitee)
-        wget -O /tmp/2.3.kehou-2/signal.c https://gitee.com/coconut_floss/EduCoder_OperationSys/raw/main/Source/2.3.kehou-2/signal.c
-        wget -O /tmp/2.3.kehou-2/system_call.s https://gitee.com/coconut_floss/EduCoder_OperationSys/raw/main/Source/2.3.kehou-2/system_call.s
+        wget -O /tmp/2.3.kehou-3/signal.c https://gitee.com/coconut_floss/EduCoder_OperationSys/raw/main/Source/2.3.kehou-3/signal.c
+        wget -O /tmp/2.3.kehou-3/system_call.s https://gitee.com/coconut_floss/EduCoder_OperationSys/raw/main/Source/2.3.kehou-3/system_call.s
 
         ;;
     Github)
-        wget -O /tmp/2.3.kehou-2/signal.c https://raw.githubusercontent.com/gaobobo/EduCoder_OperationSys/main/Source/2.3.kehou-2/signal.c
-        wget -O /tmp/2.3.kehou-2/system_call.s https://raw.githubusercontent.com/gaobobo/EduCoder_OperationSys/main/Source/2.3.kehou-2/system_call.s
+        wget -O /tmp/2.3.kehou-3/signal.c https://raw.githubusercontent.com/gaobobo/EduCoder_OperationSys/main/Source/2.3.kehou-3/signal.c
+        wget -O /tmp/2.3.kehou-3/system_call.s https://raw.githubusercontent.com/gaobobo/EduCoder_OperationSys/main/Source/2.3.kehou-3/system_call.s
         ;;
     *)
         echo "Unknown download site: $DownloadSite"
@@ -30,8 +30,8 @@ rm -rf /home/headless/os/linux-0.11-lab/0/
 cd ~/os/linux-0.11-lab
 tar -zxvf ../0.tgz 0/
 
-mv -f /tmp/2.3.kehou-2/signal.c /home/headless/os/linux-0.11-lab/1/linux/kernel/signal.c
-mv -f /tmp/2.3.kehou-2/system_call.s /home/headless/os/linux-0.11-lab/1/linux/kernel/system_call.s
+mv -f /tmp/2.3.kehou-3/signal.c /home/headless/os/linux-0.11-lab/1/linux/kernel/signal.c
+mv -f /tmp/2.3.kehou-3/system_call.s /home/headless/os/linux-0.11-lab/1/linux/kernel/system_call.s
 
 cd  ~/os/linux-0.11-lab/0/linux/
 make
