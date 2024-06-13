2081986064c8270ed3609dafca2e708d6a4fcbdf
Shibo Gao <happygaoshibo@outlook.com>
2024-06-13 21:23:32 +0800

    Add 4.2.ketang

---
+++ Scripts/4.2.ketang-1/4.2.ketang-1.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+#     Operation System Course's Educoder excrises shell script. / 操作系统课程的头歌过关脚本。
+#     Copyright (C) 2024  gaobobo
+
+DownloadSite=${1:-Gitee}
+readonly DownloadSite
+
+mkdir -p /tmp/3.4.ketang-2
+mkdir -p /tmp/3.4.ketang-1
+
+case $DownloadSite in
+    Gitee)
+        wget -O /tmp/3.4.ketang-2/第二关.txt https://gitee.com/coconut_floss/EduCoder_OperationSys/raw/main/Source/3.4.ketang-2/第二关.txt
+        wget -O /tmp/3.4.ketang-2/dbg.log https://gitee.com/coconut_floss/EduCoder_OperationSys/raw/main/Source/3.4.ketang-2/dbg.log
+        wget -O /tmp/3.4.ketang-1/gdb.log https://gitee.com/coconut_floss/EduCoder_OperationSys/raw/main/Source/3.4.ketang-1/gdb.log
+        ;;
+    Github)
+        wget -O /tmp/3.4.ketang-2/第二关.txt https://raw.githubusercontent.com/gaobobo/EduCoder_OperationSys/main/Source/3.4.ketang-2/第二关.txt
+        wget -O /tmp/3.4.ketang-2/dbg.log https://raw.githubusercontent.com/gaobobo/EduCoder_OperationSys/main/Source/3.4.ketang-2/dbg.log
+        wget -O /tmp/3.4.kehou-1/gdb.log https://raw.githubusercontent.com/gaobobo/EduCoder_OperationSys/main/Source/3.4.kehou-1/gdb.log
+        ;;
+    *)
+        echo "Unknown download site: $DownloadSite"
+        echo "未知的下载站点：$DownloadSite"
+        return 1
+        ;;
+esac
+
+mv -f /tmp/3.4.ketang-2/第二关.txt /data/workspace/myshixun/第二关.txt
+mv -f /tmp/3.4.ketang-2/dbg.log /root/dbg.log
+mv -f /tmp/3.4.ketang-1/gdb.log /root/gdb.log
+
+return 0
